93cdc68cfb90ae800c6de6652a5ec1c22b3bd4e4
Andrew Hurt <andrew.hurt1@ibm.com>
2019-12-02 11:19:50 +0000

    [FABCAG-3] change regex to match FABCAG
    Signed-off-by: Andrew Hurt <andrew.hurt1@ibm.com>

--- .release/changelog.sh
+++ .release/changelog.sh
@@ -11,7 +11,7 @@ fi
 
 echo "## $2\n$(date)" >> CHANGELOG.new
 echo "" >> CHANGELOG.new
-git log ${OLD_VERSION}HEAD  --oneline | grep -v Merge | sed -e "s/\[\(FAB-[0-9]*\)\]/\[\1\](https:\/\/jira.hyperledger.org\/browse\/\1\)/" -e "s/ \(FAB-[0-9]*\)/ \[\1\](https:\/\/jira.hyperledger.org\/browse\/\1\)/" -e "s/\([0-9|a-z]*\)/* \[\1\](https:\/\/github.com\/hyperledger\/fabric-contract-api-go\/commit\/\1)/" >> CHANGELOG.new
+git log ${OLD_VERSION}HEAD  --oneline | grep -v Merge | sed -e "s/\[\(FABCAG-[0-9]*\)\]/\[\1\](https:\/\/jira.hyperledger.org\/browse\/\1\)/" -e "s/ \(FABCAG-[0-9]*\)/ \[\1\](https:\/\/jira.hyperledger.org\/browse\/\1\)/" -e "s/\([0-9|a-z]*\)/* \[\1\](https:\/\/github.com\/hyperledger\/fabric-contract-api-go\/commit\/\1)/" >> CHANGELOG.new
 echo "" >> CHANGELOG.new
 cat CHANGELOG.md >> CHANGELOG.new
 mv -f CHANGELOG.new CHANGELOG.md
